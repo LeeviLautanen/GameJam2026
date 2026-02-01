e6074487e41d59955ff200dddb883bfaf00780b7
Aino <aino.rakkolainen@gmail.com>
2026-02-01 13:44:59 +0200

    initial version of start menu and ui buttons

---
+++ Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class StartMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnStartButtonPressed(){
+		string uid = "uid://40821jecs6kb";
+		GetTree().ChangeSceneToFile(uid);
+	}
+
+	public void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+}
